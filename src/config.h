e59f50f3d35ad83f9772219468b6cdf6eda39c4b
Dominik Madarász <zaklaus@madaraszd.net>
2022-10-15 19:12:53 +0200

    Increase near clipping plane

--- src/config.h
+++ src/config.h
@@ -104,7 +104,7 @@
 
 #define RL_MAX_SHADER_LOCATIONS               32      // Maximum number of shader locations supported
 
-#define RL_CULL_DISTANCE_NEAR               0.01      // Default projection matrix near cull distance
+#define RL_CULL_DISTANCE_NEAR               0.1       // Default projection matrix near cull distance
 #define RL_CULL_DISTANCE_FAR              3000.0      // Default projection matrix far cull distance
 
 // Default shader vertex attribute names to set location points
